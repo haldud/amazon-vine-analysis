880e2c69a0d94fbcfddcb08ac3ec348705b70111
m031506 <dudakovic.halil@mayo.edu>
2022-04-23 17:15:12 -0500

    deliverable 2 column rename

--- Vine_Review_Analysis.sql
+++ Vine_Review_Analysis.sql
@@ -29,7 +29,7 @@ SELECT COUNT(review_id) AS total_paid_reviews
 FROM vine_helpful_filtered_vine;
 
 -- Paid, 5-star
-SELECT COUNT(review_id)
+SELECT COUNT(review_id) AS total_paid_5_star_reviews
 FROM vine_helpful_filtered_vine five_star
 WHERE star_rating = 5;
 
@@ -37,15 +37,15 @@ WHERE star_rating = 5;
 SELECT ROUND(CAST((SELECT COUNT(review_id)
                    FROM vine_helpful_filtered_vine five_star
                    WHERE star_rating = 5) AS NUMERIC) /
-             CAST(COUNT(review_id) AS NUMERIC) * 100, 2) AS percent_five_star
+             CAST(COUNT(review_id) AS NUMERIC) * 100, 2) AS percent_paid_five_star
 FROM vine_helpful_filtered_vine;
 
 -- Unpaid, total
-SELECT COUNT(review_id) AS total_paid_reviews
+SELECT COUNT(review_id) AS total_unpaid_reviews
 FROM vine_helpful_filtered_non_vine;
 
 -- Unpaid, 5-star
-SELECT COUNT(review_id)
+SELECT COUNT(review_id) AS total_unpaid_5_star_reviews
 FROM vine_helpful_filtered_non_vine five_star
 WHERE star_rating = 5;
 
@@ -53,5 +53,5 @@ WHERE star_rating = 5;
 SELECT ROUND(CAST((SELECT COUNT(review_id)
                    FROM vine_helpful_filtered_non_vine five_star
                    WHERE star_rating = 5) AS NUMERIC) /
-             CAST(COUNT(review_id) AS NUMERIC) * 100, 2) AS percent_five_star
+             CAST(COUNT(review_id) AS NUMERIC) * 100, 2) AS percent_unpaid_five_star
 FROM vine_helpful_filtered_non_vine;
